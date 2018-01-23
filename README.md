c9f429871445df90ffe56976391bcaa44e9cca18
H. Watanabe <kaityo@users.sourceforge.jp>
2018-01-23 12:55:11 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Grep in Microsoft Powerpoint (pptx) files
+
+## Summary
+
+Find keywords in pptx files.
+
+## Usage
+
+    $ ruby grep_pptx keyword
+    find "keyword" in ./hoge/test2.pptx at slide 5
+    find "keyword" in ./test.pptx at slide 1
+    find "keyword" in ./test2.pptx at slide 5
+
+It will search pptx files recursively from the current directly.
---
+++ grep_pptx.rb
@@ -0,0 +1,35 @@
+require 'tmpdir'
+require 'rexml/document'
+
+if ARGV.size < 1
+  puts "usage: ruby grep_pptx.rb keyword"
+  exit
+end
+keyword = ARGV[0]
+
+def mygrep(dir, keyword, inputfile)
+  Dir.glob(dir+"/ppt/slides/*.xml") do |file|
+    slidenum = 0
+    if file=~/slide([0-9]).xml/
+      slidenum = $1.to_i
+    end
+    f = open(file)
+    doc = REXML::Document.new(f.gets(nil))
+    n =  REXML::XPath.first(doc.root,'/p:sld/p:cSld')
+    text = REXML::XPath.match(n, './/text()').join
+    if text.include?(keyword)
+      puts "find \"#{keyword}\" in #{inputfile} at slide #{slidenum}"
+    end
+  end
+end
+
+def find_in_file(inputfile, keyword)
+  Dir.mktmpdir(nil,'./') do |dir|
+    `cd #{dir};unzip ../#{inputfile}`
+    mygrep(dir, keyword, inputfile)
+  end
+end
+
+Dir.glob './**/*.pptx' do |file|
+  find_in_file(file, keyword)
+end
